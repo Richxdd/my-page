5d6400e59d42922df83746c3bbd38740115f54c5
RICARDO SOLIS <ricardosv46@gmail.com>
2022-01-30 00:28:56 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="container flex justify-between p-4">
-      <button className="text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+    <div className="container lg:flex lg:justify-between p-4 ">
+      <button className="text-xs lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
         Ricardo
       </button>
-      <div className="flex gap-3">
-        <button className="text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+      <div className="flex gap-3 flex-col lg:flex-row">
+        <button className="text-xs lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
           Proyectos
         </button>
-        <button className="text-white hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+        <button className="text-xs lg:text-xl text-white hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
           Sobre mi
         </button>
-        <button className="text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+        <button className="text-xs lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
           Skills
         </button>
-        <button className="text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+        <button className="text-xs lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
           Contactarme
         </button>
       </div>
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Inicio = () => {
   return (
     <div className=" container flex flex-1 items-start justify-center flex-col px-5">
-      <p className="text-5xl text-white font-extrabold">
+      <p className="text-xl lg:text-5xl text-white font-extrabold">
         Hola, Soy Ricardo Solis
       </p>
-      <span className="text-red-600 font-extrabold text-6xl">
+      <span className="text-red-600 font-extrabold text-xl lg:text-6xl">
         Desarrollador Front End.
       </span>
     </div>
