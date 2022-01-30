fee9ea8349f009c938db6343f430dfbc59d62bf2
RICARDO SOLIS <ricardosv46@gmail.com>
2022-01-30 17:40:53 -0500

    update

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Shop from '../img/shop.png'
 
 const Proyectos = () => {
+  const proyectos = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
-    <div>
-      <p>Desde Proyectos</p>
+    <div className="container">
+      <h1 className="text-white text-5xl font-extrabold text-center py-10">
+        Mis proyectos
+      </h1>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-3 grid-rows-3 gap-10 justify-center items-center justify-items-center  ">
+          {proyectos.map((proyecto) => (
+            <div className="w-72 h-72 bg-gray-800 opacity-80 rounded-3xl flex flex-col justify-between items-center py-5 text-white font-bold">
+              <h2>React Shop</h2>
+              <img
+                src={Shop}
+                className="w-60 h-32 rounded-xl border-2 border-neutral-500"
+              />
+
+              <a href="https://www.reactshop.xyz">https://www.reactshop.xyz/</a>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Foto from '../img/foto.jpg'
 
 const SobreMi = () => {
   return (
-    <div>
-      <p>Desde Sobre mi</p>
+    <div className="container flex flex-1 items-center justify-between gap-12">
+      <img
+        src={Foto}
+        className="rounded-full  w-[35rem] border-8 border-gray-800"
+      />
+      <div className="flex flex-col gap-14 ">
+        <h1 className="text-white text-6xl font-extrabold text-center">
+          Sobre Mi
+        </h1>
+        <p className="text-white text-2xl font-bold text-center px-8">
+          Soy desarrollador de software, residente en Perú, amante de la
+          tecnología. Mi propósito principal es construir soluciones
+          tecnológicas que ayuden a facilitar la vida de las personas.
+        </p>
+      </div>
     </div>
   )
 }
