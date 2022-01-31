b46b21b39ccaa3ee30d2edbfa26f3b1b847293b4
RICARDO SOLIS <ricardosv46@gmail.com>
2022-01-31 00:34:19 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,36 +14,40 @@ const Header = () => {
       <div className="container  flex justify-between  p-4 ">
         <Link
           to="/"
-          className="text-lg lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+          className=" hover:scale-125  text-lg lg:text-xl text-white hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
           Ricardo
         </Link>
         <div className="invisible lg:visible h-0 w-0 lg:w-auto lg:h-auto   lg:order-3">
           <div className=" flex gap-3 items-end flex-col lg:flex-row lg:visible  right-0">
             <Link
               to="/proyectos"
-              className="text-xs lg:text-xl text-white md:w-28 lg:w-auto hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className=" hover:scale-125 text-xs lg:text-xl text-white md:w-28 lg:w-auto hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Proyectos
             </Link>
 
             <Link
               to="/sobremi"
-              className="text-xs lg:text-xl text-white  md:w-28 lg:w-auto hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className="hover:scale-125 text-xs lg:text-xl text-white  md:w-28 lg:w-auto hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Sobre mi
             </Link>
             <Link
               to="/skills"
-              className="text-xs lg:text-xl text-white md:w-28 lg:w-auto hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className=" hover:scale-125 text-xs lg:text-xl text-white md:w-28 lg:w-auto hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Skills
             </Link>
             <Link
               to="/contactarme"
-              className="text-xs lg:text-xl text-white  md:w-28 lg:w-auto  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className=" hover:scale-125 text-xs lg:text-xl text-white  md:w-28 lg:w-auto  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Contactarme
             </Link>
           </div>
         </div>
         <div className="lg:invisible lg:h-0 lg:w-0">
-          <img src={Menu} className=" w-7 h-7 " onClick={handleClick} />
+          <img
+            src={Menu}
+            className=" w-7 h-7 cursor-pointer "
+            onClick={handleClick}
+          />
         </div>
       </div>
       {menu ? (
@@ -51,22 +55,22 @@ const Header = () => {
           <div className="bg-gray-800 rounded-xl w-3/8 absolute z-50 flex gap-3 items-end flex-col lg:flex-row lg:visible  right-0 p-5">
             <Link
               to="/proyectos"
-              className="text-lg lg:text-xl text-white  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className="hover:scale-125 text-lg lg:text-xl text-white  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Proyectos
             </Link>
             <Link
               to="/sobremi"
-              className="text-lg lg:text-xl text-white   hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className=" hover:scale-125 text-lg lg:text-xl text-white   hover:text-gray-800 font-extrabold hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Sobre mi
             </Link>
             <Link
               to="/skills"
-              className="text-lg lg:text-xl text-white  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className="hover:scale-125 text-lg lg:text-xl text-white  hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Skills
             </Link>
             <Link
               to="/contactarme"
-              className="text-lg lg:text-xl text-white    hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
+              className="hover:scale-125 text-lg lg:text-xl text-white    hover:text-gray-800 font-extrabold  hover:bg-white rounded-full py-1 px-3 text-center ease-in duration-300 ">
               Contactarme
             </Link>
           </div>
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -9,10 +9,12 @@ import Crm from '../img/crm.png'
 import Musica from '../img/musica.png'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Navigation } from 'swiper'
+import { motion } from 'framer-motion'
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
+import { Link } from 'react-router-dom'
 
 const Proyectos = () => {
   const proyects = [
@@ -142,15 +144,17 @@ const Proyectos = () => {
       <div className="flex justify-center lg:visible invisible lg:w-auto w-0 lg:h-auto h-0">
         <div className="grid lg:grid-cols-3 lg:grid-rows-3 gap-10 justify-center items-center justify-items-center  ">
           {proyects.map((proyect) => (
-            <div className="lg:w-72 lg:h-72 bg-gray-800 opacity-80 rounded-3xl flex flex-col justify-between items-center py-5 text-white font-bold h-0 w-0'">
+            <a
+              href={proyect.link}
+              className="lg:w-72 lg:h-72 hover:scale-125 duration-200 bg-gray-800 opacity-80 hover:opacity-100 rounded-3xl flex flex-col justify-between items-center py-5 text-white font-bold h-0 w-0' cursor-pointer  ">
               <h2>{proyect.nombre}</h2>
               <img
                 src={proyect.foto}
                 className="w-60 h-32 rounded-xl border-2 border-neutral-500"
               />
 
-              <a href={proyect.link}>{proyect.link}</a>
-            </div>
+              <a>{proyect.link}</a>
+            </a>
           ))}
         </div>
       </div>
@@ -167,8 +171,8 @@ const Proyectos = () => {
           className="mySwiper">
           {proyects1.map((proyect) => (
             <SwiperSlide>
-              <div className=" flex justify-center">
-                <div className="w-60 h-60 bg-gray-800 opacity-80 rounded-3xl flex flex-col justify-between items-center py-5 text-white font-bold">
+              <div className=" flex justify-center 	">
+                <div className="w-60 h-60  bg-gray-800 opacity-80 rounded-3xl flex flex-col justify-between items-center py-5 text-white font-bold">
                   <h2>{proyect.nombre}</h2>
                   <img
                     src={proyect.foto}
